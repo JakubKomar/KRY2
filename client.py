9385a43dd6a01771855dce1c19154aef0295db7f
komca12 <57370991+komca12@users.noreply.github.com>
2023-03-18 23:09:12 +0100

    xad

--- client.py
+++ client.py
@@ -8,11 +8,9 @@ def startClient(port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, port))
 
-    client.send("I am CLIENT\n")
-    from_server = client.recv(4096)
-    print("I received from SERVER %s" %from_server)
-    print("Send your character to SERVER")
-    input1=str(input())
-    client.send(input1)
+    toSend=input("Zadej zprávu:")
+    client.sendall(toSend.encode('utf-8'))
 
+    data_tmp = client.recv(1024)
+    print("Server response: ",data_tmp.decode('utf-8'))
     client.close()
--- server.py
+++ server.py
@@ -8,11 +8,13 @@ def startServer(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, port))
         s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    print(data.decode('utf-8'))
+                    if not data:
+                        break
+                    conn.sendall(data)
